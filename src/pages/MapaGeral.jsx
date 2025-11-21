796c7f69d1104677d96919792b1d06a7286017b0
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2025-11-21 15:39:05 +0000

    File changes

--- src/pages/MapaGeral.jsx
+++ src/pages/MapaGeral.jsx
@@ -3,6 +3,7 @@ import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
 import { Map, Square, MapPin, Minus, Layers, Filter, Save, X, Edit, Eye } from "lucide-react";
 import { toast } from "sonner";
 import {
