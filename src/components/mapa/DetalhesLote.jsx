83f8c8f9cf07bdc948d2d4017cede90a16914ad9
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2025-11-21 20:39:37 +0000

    File changes

--- src/components/mapa/DetalhesLote.jsx
+++ src/components/mapa/DetalhesLote.jsx
@@ -4,7 +4,7 @@ import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Check } from "lucide-react";
 import { base44 } from "@/api/base44Client";
-import { useQuery, useMutation, useQueryClient } from "@tantml/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast } from "sonner";
 import {
   Dialog,
