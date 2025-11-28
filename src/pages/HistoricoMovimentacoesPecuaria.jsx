9fcf358df2883d6be13fa64bf092bba4e3f647c4
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2025-11-28 11:30:29 +0000

    File changes

--- src/pages/HistoricoMovimentacoesPecuaria.jsx
+++ src/pages/HistoricoMovimentacoesPecuaria.jsx
@@ -718,6 +718,8 @@ export default function HistoricoMovimentacoesPecuaria() {
         )}
       </AnimatePresence>
 
+      {!showNovoLancamento && <SaldoCategorias movimentacoes={movimentacoes} />}
+
       {!showNovoLancamento && <Card className="shadow-sm border-slate-300">
         <CardHeader className="bg-white border-b border-slate-200 py-2 px-4">
           <div className="flex items-center justify-between gap-4">
