0df926c3289015cfd37b6b7cc30f81d2c6f7a15d
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2025-11-28 11:58:14 +0000

    File changes

--- src/pages/HistoricoMovimentacoesPecuaria.jsx
+++ src/pages/HistoricoMovimentacoesPecuaria.jsx
@@ -716,9 +716,9 @@ export default function HistoricoMovimentacoesPecuaria() {
             }}
           />
         )}
-      </AnimatePresence>
+        </AnimatePresence>
 
-      {!showNovoLancamento && <SaldoCategorias movimentacoes={movimentacoes} />}
+        {!showNovoLancamento && <SaldoCategorias movimentacoes={movimentacoes} categoriasManejo={[]} />}
 
       {!showNovoLancamento && <Card className="shadow-sm border-slate-300">
         <CardHeader className="bg-white border-b border-slate-200 py-2 px-4">
