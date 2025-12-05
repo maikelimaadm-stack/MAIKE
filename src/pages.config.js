d04b5876504017befda57535c7d98bc4e483ef75
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2025-12-05 01:08:27 +0000

    Relatório sintético com eixos

--- src/pages.config.js
+++ src/pages.config.js
@@ -56,7 +56,6 @@ import FichaControleCombustivel from './pages/FichaControleCombustivel';
 import RemoverDuplicados from './pages/RemoverDuplicados';
 import FichasPersonalizadas from './pages/FichasPersonalizadas';
 import VisualizarFicha from './pages/VisualizarFicha';
-import DashboardPublico from './pages/DashboardPublico';
 import __Layout from './Layout.jsx';
 
 
@@ -119,7 +118,6 @@ export const PAGES = {
     "RemoverDuplicados": RemoverDuplicados,
     "FichasPersonalizadas": FichasPersonalizadas,
     "VisualizarFicha": VisualizarFicha,
-    "DashboardPublico": DashboardPublico,
 }
 
 export const pagesConfig = {
