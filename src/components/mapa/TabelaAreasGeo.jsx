3b6da268e05bf5717a7e93086a2d26040add07ee
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2026-02-18 15:48:24 +0000

    File changes

--- src/components/mapa/TabelaAreasGeo.jsx
+++ src/components/mapa/TabelaAreasGeo.jsx
@@ -129,7 +129,8 @@ export default function TabelaAreasGeo({ areas, onEdit, onEditDetalhes, onDelete
   };
 
   return (
-    <Card className="shadow-sm border-slate-300">
+    <>
+      <Card className="shadow-sm border-slate-300">
       <CardHeader className="bg-white border-b border-slate-200 py-2 px-4">
         <div className="flex items-center justify-between gap-4">
           <CardTitle className="text-sm font-semibold text-slate-900 flex items-center gap-2">
