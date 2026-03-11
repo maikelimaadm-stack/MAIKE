ff4107569a71685fed9b900aa23317cb0b337c4a
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2026-03-11 13:37:11 +0000

    Manual code change

--- vite.config.js
+++ vite.config.js
@@ -2,19 +2,14 @@ import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     base44({
-      // Support for legacy code that imports the base44 SDK with @/integrations, @/entities, etc.
-      // can be removed if the code has been updated to use the new SDK imports from @base44/sdk
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true'
     }),
     react(),
   ],
   server: {
-    allowedHosts: [
-      'ta-01kkeewmsrvk843098c0m1a7yq-5173-bh58hauower12crpa0npmwl3w.w.modal.host',
-    ],
+    allowedHosts: 'all',
   },
 });
