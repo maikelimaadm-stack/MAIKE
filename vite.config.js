03ea80a89951082205a0dbadc329a82ecaf7120e
MAIKE FAZENDA PALMITAL <maike.fazendapalmital@gmail.com>
2026-03-11 13:38:21 +0000

    Manual code change

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import base44 from "@base44/vite-plugin"
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [
     base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true'
+      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === "true",
     }),
     react(),
   ],
   server: {
-    allowedHosts: 'all',
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: true,
   },
-});
+})
