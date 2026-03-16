26bd7d08d1fdd1502078d22eafe4382dc76b70ba
Maike Lima <maike.lima.adm@gmail.com>
2026-03-16 18:51:49 +0000

    File changes

--- src/components/mapa/DetalhesLote.jsx
+++ src/components/mapa/DetalhesLote.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Button } from "@/components/ui/button";
 
 import { base44 } from "@/api/base44Client";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
