2c399c27b77b1e37e018ace3d2bd5cb81d4739f1
Maike Lima <maike.lima.adm@gmail.com>
2026-03-16 17:23:38 +0000

    File changes

--- src/components/mapa/DetalhesDepositoSuplementacao.jsx
+++ src/components/mapa/DetalhesDepositoSuplementacao.jsx
@@ -256,7 +256,6 @@ export default function DetalhesDepositoSuplementacao({ deposito, onClose }) {
 
       <Dialog open={showHistorico} onOpenChange={setShowHistorico}>
         <DialogContent className="bg-background px-2 py-2 fixed left-[50%] top-[50%] z-50 grid w-full translate-x-[-50%] translate-y-[-50%] gap-4 border shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg max-w-4xl max-h-[90vh] overflow-y-auto">
-          <DialogHeader><DialogTitle className="text-sm">Histórico do Depósito</DialogTitle></DialogHeader>
           <HistoricoDepositoSuplementacao deposito={deposito} />
         </DialogContent>
       </Dialog>
